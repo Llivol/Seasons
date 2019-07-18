20c3c6abbdf1f968530c1e856b7bf45b14721685
llimona <adriaperez20@hotmail.com>
2019-07-18 20:03:46 +0200

    Remap levels

--- Seasons-project/Cheats.gd
+++ Seasons-project/Cheats.gd
@@ -17,14 +17,11 @@ func _ready():
 	print(str(">> [F5] Unkillable mode setted to: ", unkillable))
 	print(str("\n-------- LEVELS --------\n"))
 	print(str(">> [Init] Restarts current level"))
-	print(str(">> [1] Spring 03"))
-	print(str(">> [2] Spring 06"))
-	print(str(">> [3] Summer 06"))
-	print(str(">> [4] Summer 10"))
-	print(str(">> [5] Fall 09"))
-	print(str(">> [6] Fall 14"))
-	print(str(">> [7] Winter 12"))
-	print(str(">> [8] Winter 17"))
+	print(str(">> [1] Summer 10"))
+	print(str(">> [2] Winter 17"))
+	print(str(">> [3] Spring 03"))
+	print(str(">> [4] Fall 09"))
+
 	print("\n------------------------\n")
 
 func _input(event):
@@ -60,36 +57,36 @@ func _input(event):
 			print(">> Reloading scene...")
 			
 		if Input.is_action_pressed("Level_1"):
-			get_tree().change_scene("res://levels/SpringDL3.tscn")
-			print(">> Changing scene...")
-
-		if Input.is_action_pressed	("Level_2"):
-			get_tree().change_scene("res://levels/SpringDL6.tscn")
-			print(">> Changing scene...")
-
-		if Input.is_action_pressed("Level_3"):
-			get_tree().change_scene("res://levels/SummerDL6.tscn")
-			print(">> Changing scene...")
-
-		if Input.is_action_pressed("Level_4"):
 			get_tree().change_scene("res://levels/SummerDL10.tscn")
 			print(">> Changing scene...")
 
-		if Input.is_action_pressed("Level_5"):
+		if Input.is_action_pressed	("Level_2"):
+			get_tree().change_scene("res://levels/WinterDL17.tscn")
+			print(">> Changing scene...")
+
+		if Input.is_action_pressed("Level_3"):
+			get_tree().change_scene("res://levels/SpringDL3.tscn")
+			print(">> Changing scene...")
+
+		if Input.is_action_pressed("Level_4"):
 			get_tree().change_scene("res://levels/FallDL9.tscn")
 			print(">> Changing scene...")
 
-		if Input.is_action_pressed("Level_6"):
-			get_tree().change_scene("res://levels/FallDL14.tscn")
-			print(">> Changing scene...")
-
-		if Input.is_action_pressed("Level_7"):
-			get_tree().change_scene("res://levels/WinterDL12.tscn")
-			print(">> Changing scene...")
-
-		if Input.is_action_pressed("Level_8"):
-			get_tree().change_scene("res://levels/WinterDL17.tscn")
-			print(">> Changing scene...")
+#		if Input.is_action_pressed("Level_5"):
+#			get_tree().change_scene("res://levels/FallDL9.tscn")
+#			print(">> Changing scene...")
+#
+#		if Input.is_action_pressed("Level_6"):
+#			get_tree().change_scene("res://levels/FallDL14.tscn")
+#			print(">> Changing scene...")
+#
+#		if Input.is_action_pressed("Level_7"):
+#			get_tree().change_scene("res://levels/WinterDL12.tscn")
+#			print(">> Changing scene...")
+#
+#		if Input.is_action_pressed("Level_8"):
+#			get_tree().change_scene("res://levels/WinterDL17.tscn")
+#			print(">> Changing scene...")
 		
 		# Remove the current level
 		#var level = root.get_node("Level")
